28b44a9b6ef8113554dccdb8d257969c806d5366
Basir Ahmed Shah <Basirshah@users.noreply.github.com>
2020-04-04 20:08:00 +0500

    Controller Api login page done

--- src/jfxml_login/FXMLDocumentController.java
+++ src/jfxml_login/FXMLDocumentController.java
@@ -5,12 +5,19 @@
  */
 package jfxml_login;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
 /**
  *
@@ -18,18 +25,76 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField emailIdField;
     
-//  @FXML
-//    private void handleButtonAction(ActionEvent event) {
-//        System.out.println("You clicked me!");
-//        label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+   }
+    public void login(ActionEvent event) throws SQLException {
+
+        Window owner = submitButton.getScene().getWindow();
+
+        System.out.println(emailIdField.getText());
+        System.out.println(passwordField.getText());
+
+        if (emailIdField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter your email id");
+            return;
+        }
+        if (passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter a password");
+            return;
+        }
+
+        String emailId = emailIdField.getText();
+        String password = passwordField.getText();
+
+//        JdbcDao jdbcDao = new JdbcDao();
+//        boolean flag = jdbcDao.validate(emailId, password);
+//
+//        if (!flag) {
+//            infoBox("Please enter correct Email and Password", null, "Failed");
+//        } else {
+//            infoBox("Login Successful!", null, "Failed");
+//        }
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+       alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+       alert.initOwner(owner);
+        alert.show();
+    }
+
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 //    }
-//    
+
+  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
-}
+    } }   
